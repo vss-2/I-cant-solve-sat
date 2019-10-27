9a733c0edc89c12b60e417b55c5543d99c9e1364
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-10-26 23:27:05 -0300

    Legibility update
    Removidas linhas em branco, identação concisa e condensação de console.log()

--- satcode.js
+++ satcode.js
@@ -1,111 +1,88 @@
+const fs = require("fs");
 
-const fs = require("fs")
-
-var linhas = fs.readFileSync('/Users/lucas/eclipse-workspace/castor/hole1.cnf').toString()
+var linhas = fs.readFileSync('./hole1.cnf').toString();
 var x = 0;
 
-var arraes = []
+var arraes = [];
 arraes = linhas.split("\n");
+
 while (x < arraes.length) {
-
 	if (arraes[x].charAt(0) == 'c') {
-
 		arraes[x] = "";
-
 		x++;
 	} else {
 		x++;
 	}
-
 }
 
 x = 0;
 var z = 0;
 transpo = [];
-while (x <= arraes.length) {
 
-	if (arraes[x] != ("")) {
+while(x <= arraes.length){
+	if(arraes[x] != ("")){
 		transpo[z] = arraes[x];
 		z++;
 	}
-
 	x++;
-
 }
 
-arraes = transpo[0].replace("p", "").replace("cnf", "").replace("  ", "")
-		.split(" ");
+arraes = transpo[0].replace("p", "").replace("cnf", "").replace("  ", "").split(" ");
 
 var avariables = parseInt(arraes[0]);
-
 var aclauses = parseInt(arraes[1]);
 
-
-var clauses= [];
+var clauses = [];
 var variables = [];
 x=0;
-while(x<avariables){
-	
-variables[x]=false;	
-	
 
+while(x < avariables){	
+  variables[x] = false;	
 	x++;
 }
 
 x=0;
 
-while(x<transpo.length-2){
-		
-clauses[x]=transpo[x+1].split(" ");
-x++;
+while(x < transpo.length-2){		
+  clauses[x]=transpo[x+1].split(" ");
+  x++;
 }
+
 x=0;
 console.log("{ clauses: [");
 
-
-while(x<clauses.length){
-	
-	console.log( clauses[x] ) 
-	
-
+while(x < clauses.length){
+	console.log(clauses[x]);
 	x++;
 }
-console.log("] ")
-console.log("variables: ") + console.log(variables)
-console.log("}")
-c =true
-b=false
-e=true
 
+console.log("] " + "\n" + "variables: " + "\n" + variables + "\n" + "}");
 
-console.log(b & c )
+c = true;
+b = false;
+e = true;
 
+console.log(b & c);
 
+x = 0;
+n = 3;
 
- x = 0;
-		 n = 3;
-		var combinacoes = new Array();
-		
-		while (x < Math.pow(2, n)) {
-			z=0;
-		   
-		   p = (parseInt(x++, 10).toString(2)) ;
-		if(p.length%n!=0){
-			
-			while(p.length%n!=0){
-				p = "0"+p
-			}
-		
-		}
+var combinacoes = new Array();
 
-		while(z<n){
-			
-			combinacoes[x-1]= p;
-			
-				
-			
-				z++
-			}
-		
-		}
-		console.log(combinacoes)
+while (x < Math.pow(2, n)) {
+	z=0;
+   
+  p = (parseInt(x++, 10).toString(2));
+
+  if(p.length%n != 0){
+	  while(p.length%n !=0){
+	  	p = "0"+p
+	  }
+}
+
+while(z < n){
+	combinacoes[x-1]= p;
+		z++;
+	}
+}
+console.log(combinacoes);
